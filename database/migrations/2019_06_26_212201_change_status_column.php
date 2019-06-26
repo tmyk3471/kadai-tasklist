c5be08e0eaa1e7597e442e2241b69332230f8c3a
tmyk3471 <tmyk3471@gmail.com>
2019-06-26 12:32:32 +0000

    add_migration2

--- database/migrations/2019_06_26_212201_change_status_column.php
+++ database/migrations/2019_06_26_212201_change_status_column.php
@@ -4,7 +4,7 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Database\Migrations\Migration;
 
-class AddStatusToTasksTable extends Migration
+class ChangeStatusColumn extends Migration
 {
     /**
      * Run the migrations.
