3ecf86555c39d48b83df2a991f1bd4e9e7d4c4b8
tmyk3471 <tmyk3471@gmail.com>
2019-06-28 13:03:20 +0000

    user pages

--- routes/web.php
+++ routes/web.php
@@ -11,25 +11,23 @@
 |
 */
 
-Route::get('/', 'TasksController@index');
+Route::get('/', function () {
+    return view('welcome');
+});
 
-/*
-// CRUD
-// メッセージの個別詳細ページ表示
-Route::get('tasks/{id}', 'TasksController@show');
-// メッセージの新規登録を処理（新規登録画面を表示するためのものではありません）
-Route::post('tasks', 'TasksController@store');
-// メッセージの更新処理（編集画面を表示するためのものではありません）
-Route::put('tasks/{id}', 'TasksController@update');
-// メッセージを削除
-Route::delete('tasks/{id}', 'TasksController@destroy');
+// ユーザ登録
+Route::get('signup', 'Auth\RegisterController@showRegistrationForm')->name('signup.get');
+Route::post('signup', 'Auth\RegisterController@register')->name('signup.post');
 
-// index: showの補助ページ
-Route::get('tasks', 'TasksController@index')->name('tasks.index');
-// create: 新規作成用のフォームページ
-Route::get('tasks/create', 'TasksController@create')->name('tasks.create');
-// edit: 更新用のフォームページ
-Route::get('tasks/{id}/edit', 'TasksController@edit')->name('tasks.edit');
-*/
+// ログイン認証
+Route::get('login', 'Auth\LoginController@showLoginForm')->name('login');
+Route::post('login', 'Auth\LoginController@login')->name('login.post');
+Route::get('logout', 'Auth\LoginController@logout')->name('logout.get');
+
+// ユーザ機能
+Route::group(['middleware' => ['auth']], function () {
+    Route::resource('users', 'UsersController', ['only' => ['index', 'show']]);
+});
+
 
 Route::resource('tasks', 'TasksController');
